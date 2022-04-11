fc6f16fbc9a42181173b080ddcc93b0d849c0203
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-11 01:52:35 +0000

    [fix]: changed format string to avoid [-wformat=] issues

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	int i, li, lli;
 	float fl;
 
-	printf("Size of a char: %c byte(s)\n", sizeof(c));
-	printf("Size of an int: %i byte(s)\n", sizeof(i));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %li byte(s)\n", (unsigned long)sizeof(li));
 	printf("Size of a long long int: %lli byte(s)\n", (unsigned long long)sizeof(lli));
-	printf("Size of a float: %f byte(s)\n", sizeof(fl));
+	printf("Size of a float: %lu byte(s)\n", sizeof(fl));
 
 	return (0);
 }
