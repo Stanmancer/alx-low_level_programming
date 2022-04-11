b3e01456adebb1e9fe671a0c05976802ed45f88a
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-11 01:29:04 +0000

    [fix]: placed missing parenthesis ()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %c byte(s)\n", sizeof(c));
 	printf("Size of an int: %i byte(s)\n", sizeof(i));
-	printf("Size of a long int: %li byte(s)\n", (long)sizeof(li);
-	printf("Size of a long long int: %lli byte(s)\n"), (long long)sizeof(lli);
+	printf("Size of a long int: %li byte(s)\n", (long)sizeof(li));
+	printf("Size of a long long int: %lli byte(s)\n", (long long)sizeof(lli));
 	printf("Size of a float: %f byte(s)\n"' sizeof(f));
 
 	return (0);
