9d6ad666a45981477343ce0069b8b9e7930e4df3
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-11 01:57:07 +0000

    [fix]: betty style line characters check

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %li byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lli byte(s)\n", (unsigned long long)sizeof(lli));
+	printf("Size of a long long int: %lli byte(s)\n", (long long)sizeof(lli))
 	printf("Size of a float: %lu byte(s)\n", sizeof(fl));
 
 	return (0);
