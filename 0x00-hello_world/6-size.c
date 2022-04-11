a762a12c3b0674ddec2c7938e95847bb962eef7d
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-11 01:26:10 +0000

    task 6 display size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * printf - prints specified string
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	int i, li, lli;
+	float f;
+
+	printf("Size of a char: %c byte(s)\n", sizeof(c));
+	printf("Size of an int: %i byte(s)\n", sizeof(i));
+	printf("Size of a long int: %li byte(s)\n", (long)sizeof(li);
+	printf("Size of a long long int: %lli byte(s)\n"), (long long)sizeof(lli);
+	printf("Size of a float: %f byte(s)\n"' sizeof(f));
+
+	return (0);
+}
