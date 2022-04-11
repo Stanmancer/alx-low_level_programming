8d6a33e2fd40a6306818e0426263616ad0bfe181
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-11 02:20:28 +0000

    [fix]: i need leep

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,8 +11,8 @@ int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %li byte(s)\n", (long)sizeof(int));
-	printf("Size of a long long int: %lli byte(s)\n", (long long)sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (long)sizeof(int));
+	printf("Size of a long long int: %lu byte(s)\n", (long long)sizeof(int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
