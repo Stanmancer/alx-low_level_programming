6769a1a717120d015fcb81fdbd582450793e3688
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-16 19:55:00 +0000

    printing a complex string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * printf: displays a string
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
