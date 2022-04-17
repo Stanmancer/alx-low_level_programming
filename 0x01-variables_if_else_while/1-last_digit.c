fcd6c4c7ff43b07141a2240f7343a8124ad0e6c5
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-17 18:35:19 +0000

    [fix]:added and operator (&&) for combining relational statement [trial 2]

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
-	else if (lastDigit < 6, lastDigit != 0)
+	else if (lastDigit < 6 && lastDigit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
