ad96974f57ab39751cfc2ce822e80ea019462c04
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-24 01:49:27 +0000

    0-putchar.c [trial 1]

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 means sucess
+ */
+
+int main(void)
+{
+	char c = '_';
+
+	do
+	{
+		_putchar(c);
+		if (c == '_')
+			c = 'p';
+		else if (c == 'p')
+			c = 'u';
+		else if (c == 'u')
+			c = 't';
+		else if (c == 't')
+			c = 'c';
+		else if (c == 'c')
+			c = 'h';
+		else if ( c == 'h')
+			c = 'a';
+		else if (c == 'a')
+			c = 'r';
+		else if (c == 'r')
+			c = 'z';
+	} while (c != 'z');
+
+	return (0);
+}
