48acbe6bae6f6cdeff622be12eb6bd0e8a03e296
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-24 16:56:36 +0000

    prints every minute of the day from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "maiin.h"
+
+/**
+ * jack_bauer - prints every minute from 00:00 to 23:59
+ *
+ * return: always 1 success
+ */
+
+void jack_bauer(void)
+{
+	int i = 0;
+	int a;
+
+	if (i < 24)
+	{
+		for (a = 0; a < 60; a++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar('\n');
+		}
+
+		if (a == 59)
+		{
+			a = 0;
+			i++;
+		}
+	}
+}
