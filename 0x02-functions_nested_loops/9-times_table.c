2e30ddd607595408fea2ca061ccecb7a8a50baf6
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-24 18:04:27 +0000

    [fix]: double digits output error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,13 +15,32 @@ void times_table(void)
 	{
 		for (i = 0; i <= 9; i++)
 		{
-			_putchar((n * i) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			r = n * i;
+
+			if (r <= 9)
+			{
+				if (i == 0 && r == 0)
+					_putchar(r + '0');
+				else
+				{
+					_putchar(' ');
+					_putchar(r + '0');
+				}
+			}
+			else
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+
+
+			if (i < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
 		}
-
-		_putchar('\n');
-
 	}
 }
