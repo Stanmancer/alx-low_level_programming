3a9aefa769746927f2ab298f85793bff22653bdf
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-26 17:42:54 +0000

    [fix]: double output for case 98 and position of ", " in output

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - print all natural numbers from n to 98 in order
+* print_to_98 - print all natural numbers from n to 98 in order
  * @n: number passed to function
  *
  * return: always 1
@@ -11,64 +11,63 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	for (i = n; i <= 98; i++)
 	{
-		for (i = n; i <= 98; i++)
+		if (i < 0)
 		{
-			if (i < 0)
-			{
-				_putchar('-');
-				i = (i * -1);
+			_putchar('-');
+			i = (i * -1);
 
-				if (i >= 100)
-				{
-					_putchar((i / 100) + '0');
-					_putchar(((i / 10) % 10) + '0');
-					_putchar((i % 10) + '0');
-				}
-
-				else if (i < 100 && i >= 10)
-				{
-					_putchar((i / 10) + '0');
-					_putchar((i % 10) + '0');
-				}
-
-				else if (i < 10)
-					_putchar(i + '0');
-			}
-
-			else if (i > 0 && i < 10)
-				_putchar(i + '0');
-
-			else if (i > 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-		}
-	}
-
-	if (n >= 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
 			if (i >= 100)
 			{
 				_putchar((i / 100) + '0');
 				_putchar(((i / 10) % 10) + '0');
 				_putchar((i % 10) + '0');
-			}
-
-			else
+			} else if (i < 100 && i >= 10)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
-			}
+			} else if (i < 10)
+				_putchar(i + '0');
+		} else if (i > 0 && i < 10)
+			_putchar(i + '0');
+		else if (i > 10)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		while (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 
-	while (n != 98)
+	greater_than_98();
+}
+
+/**
+ * greater_than_98 - computes for values greater than 98
+ *
+ * return: always 1
+ */
+
+void greater_than_98(void)
+{
+	int n, i;
+
+	for (i = n; i > 98; i--)
 	{
+		if (i >= 100)
+		{
+			_putchar((i / 100) + '0');
+			_putchar(((i / 10) % 10) + '0');
+			_putchar((i % 10) + '0');
+		} else
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
 		_putchar(',');
 		_putchar(' ');
 	}
