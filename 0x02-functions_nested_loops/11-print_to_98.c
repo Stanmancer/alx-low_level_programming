2f3fd2a5931285e74bc38546009774dfbff0ac4a
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-04-27 18:27:24 +0000

    [fix]: placement of ", " in the output [trial 2]

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -41,14 +41,14 @@ void print_to_98(int n)
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
 			}
-			while (i != 98)
+			if (i != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
-		_putchar('\n');
 		greater_than_98(n);
+		_putchar('\n');
 	}
 }
 
@@ -63,23 +63,25 @@ void greater_than_98(int n)
 {
 	int i;
 
-	for (i = n; i >= 98; i--)
+	if (i > 98)
 	{
-		if (i >= 100)
+		for (i = n; i >= 98; i--)
 		{
-			_putchar((i / 100) + '0');
-			_putchar(((i / 10) % 10) + '0');
-			_putchar((i % 10) + '0');
-		} else
-		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-		}
-		while (i != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
+			if (i >= 100)
+			{
+				_putchar((i / 100) + '0');
+				_putchar(((i / 10) % 10) + '0');
+				_putchar((i % 10) + '0');
+			} else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
-	 _putchar('\n');
 }
