a53b896818e60d8f74d4a3ad4589de6152cf0322
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-05-01 15:30:09 +0000

    function to print triangle with given size

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle with given size
+ * @size: number passed to function as size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int s; /*for space iteration*/
+	int h; /*for # iteration*/
+	int i; /*for vertical iteration*/
+
+	if (size > 0)
+	{
+		for (h = 1; h <= size; h++)
+		{
+			s = size - h;
+			do {
+				_putchar(' ');
+				s--;
+			} while (s != 0);
+			for (i = h; i > 0; i--)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
