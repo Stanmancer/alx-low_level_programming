8fbb88e8efa4aeea525bb45366a6134305251621
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-05-07 17:39:31 +0000

    Readme and task 1 pushed

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value of the parameter the pointer points to
+ * @n: a pointer to the the integer to be changed
+ *
+ * return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
