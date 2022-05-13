4f2625310ee9c7f360dc2435e3f6345282eab2ae
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-05-13 21:29:23 +0000

    funtion to concatenate two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - function to concatenate two strings using at most 'n' bytes
+ * @dest: string 1, destination for n bytes of string 2
+ * @src: string 2 to be concatenated with string 1
+ * @n: number of bytes to be concatenated from string 2
+ *
+ * Return: the resulting string "dest"
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{}
+
+	for (b = 0; b <= n - 1; b++)
+	{
+		dest[a] = src[b];
+		a++;
+	}
+	return (dest);
+}
