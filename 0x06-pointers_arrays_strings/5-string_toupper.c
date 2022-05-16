33c8977b8a1fae48248a69f41c5c58f4269dbc41
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-05-16 12:56:09 +0000

    [fix]: changed while loop to for loop

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,14 +10,11 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	i = 0;
-
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - ' ';
-			i++;
 		}
 	}
 	return (str);
