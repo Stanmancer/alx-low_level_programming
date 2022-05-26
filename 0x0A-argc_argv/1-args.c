460a240e2d20295fceba03d7b7602ba4f9691134
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-05-26 14:44:38 +0000

    [fix betty style]: description for function main

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: array of pointers to char
+ *
+ * Return: 0
+ */
+
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
