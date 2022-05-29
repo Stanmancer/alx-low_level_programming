b2abb83657ab0ff963c828ff531486a24b58298b
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-05-29 13:16:08 +0000

    [fix]: treating NULL passed to function as empty string and corrected size of allocated memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,21 +18,22 @@ char *str_concat(char *s1, char *s2)
 	int m;
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{}
 	for (j = 0; s2[j] != '\0'; j++)
 	{}
-	i -= 1;/* removes '\0' char at end of s1 */
 
-	ptr = malloc(sizeof(char) * (i + j));
+	ptr = malloc(sizeof(char) * (i + j + 1));
 
 	if (ptr == NULL)
 		return (NULL);
 
-	for (k = 0; k <= i; k++)
+	for (k = 0; k < i; k++)
 		ptr[k] = s1[k];
 	for (m = 0; m <= j; m++)
 	{
