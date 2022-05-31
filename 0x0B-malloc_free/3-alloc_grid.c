2d9b01bfb2834662b281da9416c2d1bbcd485dfa
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-05-31 08:49:57 +0000

    function to return a pointer to a 2d array of int and initialize each element with 0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2d array of integers
+ * @width: no of coloums in the matrix
+ * @height: no of rows in the matrix
+ *
+ * Return: NULL on failure. on success, return pointer to array.
+ *	if width || height <= 0, return NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int m;
+	int n;
+	int **ptr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * height);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+
+		if (ptr[i] == NULL)
+		{
+			free(ptr[i]);
+			return (NULL);
+		}
+	}
+
+	for (m = 0; m < height; m++)
+		for (n = 0; n < width; n++)
+			ptr[m][n] = 0;
+
+	return (ptr);
+}
