86339b3b0e8664bfa3995ccbe37188eab84bdcde
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-06-17 20:39:42 +0000

    defined stuct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a Dog's bio data
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
