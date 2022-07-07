cbd11c295722ce4aea71be0c8864587afb0860d6
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-07-07 01:43:38 +0000

    [fix]: check if arguments passed to function is NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - a function that prints a name
@@ -10,5 +11,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
