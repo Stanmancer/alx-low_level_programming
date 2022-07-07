5c367ca5a294d77ad219a7502b697f50bed8ccf9
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-07-07 02:00:08 +0000

    [1-array_iterator]: executes a function given as parameter on each element of an array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
 #ifndef FUNC_POINT_H
 #define FUNC_POINT_H
 
+#include <stddef.h>
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
