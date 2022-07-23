8720bdc66705934d08be4408a869fb52592d03d4
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-07-23 22:50:11 +0000

    '[fix]: bug fixes + betty style'         - increase sizeof range for error checking 	- betty style code: space required after 'return'

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,8 +14,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	int num;
 	unsigned long int mask = 1;
 
-	if (index > (sizeof(unsigned long int)) * 2)
-		return(-1);
+	if (index > (sizeof(unsigned long int)) * 4)
+		return (-1);
 	mask = mask << index;
 	num = n & mask;
 	num = num >> index;
