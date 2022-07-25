0450b7cfc8d6a950a00934a7e3e0b81071313769
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-07-25 01:23:29 +0000

    bug fixes + edge case fix 	- corrected return values for success and errors 	- added code for edge case when index greater than size of ULONG_MAX

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,12 +13,14 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1;
 
+	if (index > (sizeof(unsigned long int) * 4))
+		return (-1);
 	mask = mask << index;
 	(*n) = (*n) | mask;
 
 	if (n[index] == 1)
-		return (1);
-	if (n[index] != 1)
 		return (-1);
+	if (n[index] != 1)
+		return (1);
 	return (0);
 }
