35e81eb8e2984d2698f44820a3de0152fb6422db
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2022-08-27 19:55:43 +0000

    [feat]: delete a node at index of a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ * listint_len - returns the number of elements of a linked listint_t list
+ * @h: pointer to HEAD of the list
+ *
+ * Return: number of elements in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+/**
  * delete_nodeint_at_index - deletes the node at index of a linked list
  * @head: double pointer to the firsy node of the list
  * @index: iindex of the node to be deleted, index starts at 0
@@ -13,6 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *node = (*head);
 	listint_t *previous = (*head);
 	unsigned int i = 0;
+	size_t len = listint_len(*head);
 
 	if (*head == NULL)
 		return (-1);
@@ -21,8 +41,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		(*head) = node->next;
 		free(node);
 		node = NULL;
+
+		return (1);
 	}
-	else
+	else if (index < len)
 	{
 		while (i < index)
 		{
@@ -34,7 +56,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		previous->next = node->next;
 		free(node);
 		node = NULL;
+
+		return (1);
 	}
 
-	return (1);
+	return (-1);
 }
