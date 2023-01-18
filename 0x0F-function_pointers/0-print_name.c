853a07d378f3b526a312e6fe9c0ca177d4f8022e
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2023-01-18 17:15:49 +0000

    [fix]: betty doc errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,13 @@
  * print_name - prints a name with a function pointer passed to it
  * @name: name of the person
  * @f: function passed to 'print_name'
+ *
+ * Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	void (*ptr)(char *) = f;
+
 	ptr(name);
 }
