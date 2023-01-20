24c34ff457bbbf3aa4ac014be8dae60294e82b0f
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2023-01-20 20:25:25 +0000

    [fix]: compilation error due to semicolon (';') ommission

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(args, char *);
 
 		if (str == NULL)
-			printf("(nil)")
+			printf("(nil)");
 
 		printf("%s", str);
 
