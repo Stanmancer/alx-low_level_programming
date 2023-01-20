c656b1e2ce99f2c5471c561369b3bc2ff3bfc2a1
Stanmancer <92708644+Stanmancer@users.noreply.github.com>
2023-01-20 20:20:05 +0000

    [add]: variaduc function to prints strings passed as arguments

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings passed as arguments
+ * @separator: string to be printed between ste strings
+ * @n: number of strings passed to function
+ *
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			continue;
+
+		printf("%s", str);
+
+		if ((i < n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,7 @@
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /*VAR_FUNC_H*/
